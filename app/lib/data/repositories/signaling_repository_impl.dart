f39bfa8ec736c5602063af117825f27ab20276b0
Farook Jamal <farookyj10@gmail.com>
2026-07-06 12:13:46 +0530

    chore(app): implement WebSocket connection and message handling in SignalingRepositoryImpl

--- app/lib/data/repositories/signaling_repository_impl.dart
+++ app/lib/data/repositories/signaling_repository_impl.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:developer';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import '../../domain/models/signaling_message.dart';
@@ -9,23 +11,52 @@ final signalingRepoProvider = Provider<SignalingRepository>((ref) {
 });
 
 class SignalingRepositoryImpl implements SignalingRepository {
+  WebSocketChannel? _channel;
+  final _messageController = StreamController<SignalingMessage>.broadcast();
+
   @override
   Future<void> connect(String url) async {
-    throw UnimplementedError('connect() not implemented yet');
+    _channel = WebSocketChannel.connect(Uri.parse(url));
+    await _channel!.ready; // Wait for connection to be established
+
+    _channel!.stream.listen(
+      (data) {
+        try {
+          final Map<String, dynamic> jsonMap = jsonDecode(data);
+          final message = SignalingMessage.fromJson(jsonMap);
+          _messageController.add(message);
+        } catch (e) {
+          log('Error parsing incoming message: $e');
+        }
+      },
+      onError: (error) {
+        log('WebSocket error: $error');
+      },
+      onDone: () {
+        log('WebSocket closed');
+      },
+    );
   }
 
   @override
   void disconnect() {
-    throw UnimplementedError('disconnect() not implemented yet');
+    _channel?.sink.close();
+    _channel = null;
   }
 
   @override
   void sendMessage(SignalingMessage message) {
-    throw UnimplementedError('sendMessage() not implemented yet');
+    if (_channel != null) {
+      final jsonString = jsonEncode(message.toJson());
+      _channel!.sink.add(jsonString);
+    }
   }
 
   @override
-  Stream<SignalingMessage> get onMessageReceived {
-    throw UnimplementedError('onMessageReceived not implemented yet');
+  Stream<SignalingMessage> get onMessageReceived => _messageController.stream;
+
+  void dispose() {
+    _messageController.close();
+    disconnect();
   }
 }
